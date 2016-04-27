9f18a5fb156d39df19e02d825ad997d6134636aa
Greyson Parrelli <greyson.parrelli@gmail.com>
2016-04-26 22:01:46 -0700

    Added game screens and audio toggle.

--- game.js
+++ game.js
@@ -7,14 +7,18 @@ this.game = this.game || {};
   var KEY_LEFT = 37;
   var KEY_RIGHT = 39;
   var KEY_SPACE = 32;
+  var KEY_M = 77;
 
   var STAGE_WIDTH = 10;
   var STAGE_HEIGHT = 10;
   var BLINK_FASTEST = 50;
   var BLINK_SLOWEST = 1000;
   var MAX_DISTANCE = Math.sqrt(STAGE_WIDTH * STAGE_WIDTH + STAGE_HEIGHT * STAGE_HEIGHT);
+  var GAME_DURATION = 30 * 1000;
 
   var _pixel = null;
+  var _titleScreen = null;
+  var _gameOverScreen = null;
   var _colorOn = '#ffffff';
   var _colorOff = '#000000';
   var _blinkRate = 0;
@@ -23,71 +27,131 @@ this.game = this.game || {};
   var _nextToggleTime = 0;
   var _onTarget = false;
   var _sound = null;
+  var _muted = true;
   var _targets = [];
+  var _state = 0; // 0 = title, 1 = game, 2 = game over
+  var _gameTimerId = 0;
+  var _score = 0;
   var _player = {
-    x: Math.round(STAGE_WIDTH / 2),
-    y: Math.round(STAGE_HEIGHT / 2)
+    x: 0,
+    y: 0
   }
 
 
   function init() {
-    // Find element
+    // Find elements
     _pixel = document.getElementById('pixel');
+    _titleScreen = document.getElementById('title');
+    _gameOverScreen = document.getElementById('game-over');
+
+    // Hide unnecessary elements
+    _gameOverScreen.style.visibility = 'hidden';
 
     // Load beep
     _sound = new Howl({
       urls: ['beep.wav']
     });
 
-    // Add event listener
+    // Add event listeners
+    _pixel.addEventListener('click', onPixelClick, false);
     window.addEventListener('keyup', onKeyUp, false);
     window.addEventListener('keydown', onKeyDown, false);
+  }
+
+  function initGame() {
+    // Hide other screens
+    _titleScreen.style.visibility = 'hidden';
+    _gameOverScreen.style.visibility = 'hidden';
+
+    // Reset score
+    _score = 0;
+
+    // Init player in the middle of the grid
+    _player.x = Math.round(STAGE_WIDTH / 2);
+    _player.y = Math.round(STAGE_HEIGHT / 2);
 
     // Add a target to the list
+    targets = [];
     spawnTarget();
 
     // Initialize update loop
-    setInterval(update, 1);
+    _gameTimerId = setInterval(update, 1);
 
     // Kick it off
     _lastToggleTime = _nextToggleTime = Date.now();
     updateBlinkRate();
+
+    // Set game length
+    setTimeout(gameOver, GAME_DURATION);
   }
 
   function onKeyUp(e) {
+    // Only do stuff while the game is running
+    if (_state != 1) return;
+
     switch(e.keyCode) {
       case KEY_UP:
         _player.y = Math.max(_player.y - 1, 0);
         updateBlinkRate();
+        e.preventDefault();
         break;
 
       case KEY_DOWN:
         _player.y = Math.min(_player.y + 1, STAGE_HEIGHT);
         updateBlinkRate();
+        e.preventDefault();
         break;
 
       case KEY_LEFT:
         _player.x = Math.max(_player.x - 1, 0);
         updateBlinkRate();
+        e.preventDefault();
         break;
 
       case KEY_RIGHT:
         _player.x = Math.min(_player.x + 1, STAGE_WIDTH);
         updateBlinkRate();
+        e.preventDefault();
         break;
 
       case KEY_SPACE:
         if (_onTarget) {
           disarm();
         }
+        e.preventDefault();
+        break;
+
+      case KEY_M:
+        _muted = !_muted;
+        e.preventDefault();
         break;
     }
-    e.preventDefault();
   }
 
   function onKeyDown(e) {
-    e.preventDefault();
+    // Only do stuff while the game is running
+    if (_state != 1) return;
+
+    // Need to preventDefault to stop window from scrolling
+    switch(e.keyCode) {
+      case KEY_UP:
+      case KEY_DOWN:
+      case KEY_LEFT:
+      case KEY_RIGHT:
+      case KEY_SPACE:
+      case KEY_M:
+        e.preventDefault();
+        break;
+    }
   }
+
+  function onPixelClick() {
+    if (_state == 0 || _state == 2) {
+      _state = 1;
+      initGame();
+    }
+  }
+
 
   function update() {
     var time = Date.now();
@@ -122,7 +186,9 @@ this.game = this.game || {};
     _pixelState = state;
     if (state) {
       _pixel.style.backgroundColor = _colorOn;
-      _sound.play();
+      if (!_muted) {
+        _sound.play();
+      }
     } else {
       _pixel.style.backgroundColor = _colorOff;
     }
@@ -143,8 +209,17 @@ this.game = this.game || {};
         break;
       }
     }
+    _score++;
     spawnTarget();
     updateBlinkRate();
+  }
+
+  function gameOver() {
+    _state = 2;
+    clearInterval(_gameTimerId);
+    setPixelState(false);
+    document.getElementById('score').innerText = _score;
+    _gameOverScreen.style.visibility = 'visible';
   }
 
   function findNearestTarget() {
